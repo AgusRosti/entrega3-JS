863bd68f1479b6e7a467f8c2737924d2d1034342
agus <agu.rostagno@hotmail.com>
2023-08-09 15:49:31 -0300

    no funciona

--- scripts/login.js
+++ scripts/login.js
@@ -1,122 +1,146 @@
 const btnIniciar = document.getElementById("sign-in");
 const btnRegistro = document.getElementById("sign-up");
-const formRegistro = document.querySelector(".registro");
-const formIngreso = document.querySelector(".ingreso");
+const formRegistros = document.querySelector(".registro");
+const formIngresos = document.querySelector(".ingreso");
 
 // formulario para registrarse
-btnIniciar.addEventListener("click", () => toggleForms(formRegistro, formIngreso));
+btnIniciar.addEventListener("click", () => toggleForms(formRegistros, formIngresos));
 
 // formulario para iniciar sesión
-btnRegistro.addEventListener("click", () => toggleForms(formIngreso, formRegistro));
+btnRegistro.addEventListener("click", () => toggleForms(formIngresos, formRegistros));
 
-// Función para alternar entre formularios
+// función para alternar formularios
 const toggleForms = (formToHide, formToShow) => {
     formToHide.classList.add("hide");
     formToShow.classList.remove("hide");
 };
 
-window.addEventListener('load', () => {
-    const form = document.getElementById("formNuevo");
-    const inputs = [
-        { element: document.getElementById("usuarioNuevo"), error: 'Campo vacío' },
-        { element: document.getElementById("contraseñaNuevo"), error: 'Campo vacío' },
-        { element: document.getElementById("contraseña2Nuevo"), error: 'Repite tu contraseña' }
-    ];
+// registro de usuarios
+const formRegistro = document.getElementById("formNuevo");
+const inputs = [
+    { element: document.getElementById("usuarioNuevo"), error: 'Campo vacío' },
+    { element: document.getElementById("contraseñaNuevo"), error: 'Campo vacío' },
+    { element: document.getElementById("contraseña2Nuevo"), error: 'Repite tu contraseña' }
+];
 
-    form.addEventListener('submit', (event) => {
-        event.preventDefault();
-        validarCampos();
-    });
+formRegistro.addEventListener('submit', (event) => {
+    event.preventDefault();
+    validarCampos();
+});
 
-    const validarCampos = () => {
-        quitarAvisos();
-        let isValid = true;
+const validarCampos = () => {
+    quitarAvisos();
+    let isValid = true;
 
-        inputs.forEach(inputData => {
-            const { element, error } = inputData;
-            const valor = element.value.trim();
+    inputs.forEach(inputData => {
+        const { element, error } = inputData;
+        const valor = element.value.trim();
 
-            if (!valor) {
-                noValida(element, error);
-                isValid = false;
-            }
-        });
-
-        if (!isValid) {
-            return; // Detener el proceso si hay campos vacíos
-        }
-
-        const usuarioValor = inputs[0].element.value.trim();
-        const passValor = inputs[1].element.value.trim();
-        const passValidaValor = inputs[2].element.value.trim();
-
-        const passInput = inputs[1].element;
-        const isPassValid = passValor.length >= 8;
-
-        if (!isPassValid) {
-            noValida(passInput, 'Debe tener 8 caracteres como mínimo');
+        if (!valor) {
+            noValida(element, error);
             isValid = false;
         }
+    });
 
-        if (!isValid) {
-            return; // Detener el proceso si alguna validación no es correcta
-        }
-
-        if (usuarioExisteEnLocalStorage(usuarioValor)) {
-            noValida(inputs[0].element, 'El usuario ya existe. Prueba con otro nombre de usuario.');
-        } else {
-            validaOk(inputs.map(inputData => inputData.element));
-            const nuevoUsuario = crearUsuario(usuarioValor, passValor);
-            guardarUsuarioEnLocalStorage(nuevoUsuario);
-        }
-    };
-
-    const usuarioExisteEnLocalStorage = (nombreUsuario) => {
-        const usuariosGuardados = JSON.parse(localStorage.getItem('usuarios')) || [];
-        return usuariosGuardados.some(usuario => usuario.nombre === nombreUsuario);
-    };
-
-    // Mostrar y eliminar avisos si son datos correctos
-    const noValida = (input, msje) => {
-        const formControl = input.parentElement;
-        const aviso = formControl.querySelector('.aviso');
-        aviso.innerText = msje;
-        formControl.classList.remove('valido');
-        formControl.classList.add('falla');
-    };
-
-    const validaOk = (inputElements) => {
-        inputElements.forEach(input => {
-            const formControl = input.parentElement;
-            formControl.classList.remove('falla');
-            formControl.classList.add('valido');
-        });
-    };
-
-    const quitarAvisos = () => {
-        const avisos = document.querySelectorAll('.aviso');
-        avisos.forEach(aviso => {
-            aviso.innerText = '';
-        });
-
-        const controles = document.querySelectorAll('.control-form');
-        controles.forEach(control => {
-            control.classList.remove('falla', 'valido');
-        });
-    };
-
-    // Guardar datos de usuarios nuevos
-    function crearUsuario(nombre, contraseña) {
-        return {
-            nombre: nombre,
-            contraseña: contraseña
-        };
+    // parar si hay campos vacíos
+    if (!isValid) {
+        return; 
     }
 
-    function guardarUsuarioEnLocalStorage(usuario) {
-        // Verifica si ya existe información en el local storage
-        const usuariosGuardados = JSON.parse(localStorage.getItem('usuarios')) || [];
-        usuariosGuardados.push(usuario);
-        localStorage.setItem('usuarios', JSON.stringify(usuariosGuardados));
+    const usuarioValor = inputs[0].element.value.trim();
+    const passValor = inputs[1].element.value.trim();
+    const passValidaValor = inputs[2].element.value.trim();
+
+    const passInput = inputs[1].element;
+    const isPassValid = passValor.length >= 8;
+
+    if (!isPassValid) {
+        noValida(passInput, 'Debe tener 8 caracteres como mínimo');
+        isValid = false;
+    }
+
+    // parar si alguna validación no es correcta
+    if (!isValid) {
+        return; 
+    }
+
+    if (usuarioExisteEnLocalStorage(usuarioValor)) {
+        noValida(inputs[0].element, 'El usuario ya existe. Prueba con otro nombre de usuario.');
+    } else {
+        validaOk(inputs.map(inputData => inputData.element));
+        const nuevoUsuario = crearUsuario(usuarioValor, passValor);
+        guardarUsuarioEnLocalStorage(nuevoUsuario);
+        toggleForms(formRegistros, formIngresos); // para intercambiar formularios e iniciar sesión
+    }
+};
+
+const usuarioExisteEnLocalStorage = (nombreUsuario) => {
+    const usuariosGuardados = JSON.parse(localStorage.getItem('usuarios')) || [];
+    return usuariosGuardados.some(usuario => usuario.nombre === nombreUsuario);
+};
+
+// Mostrar y eliminar avisos si son datos correctos
+const noValida = (input, msje) => {
+    const formControl = input.parentElement;
+    const aviso = formControl.querySelector('.aviso');
+    aviso.innerText = msje;
+    formControl.classList.remove('valido');
+    formControl.classList.add('falla');
+};
+
+const validaOk = (inputElements) => {
+    inputElements.forEach(input => {
+        const formControl = input.parentElement;
+        formControl.classList.remove('falla');
+        formControl.classList.add('valido');
+    });
+};
+
+const quitarAvisos = () => {
+    const avisos = document.querySelectorAll('.aviso');
+    avisos.forEach(aviso => {
+        aviso.innerText = '';
+    });
+
+    const controles = document.querySelectorAll('.control-form');
+    controles.forEach(control => {
+        control.classList.remove('falla', 'valido');
+    });
+};
+
+// Guardar datos de usuarios nuevos
+function crearUsuario(nombre, contraseña) {
+    return {
+        nombre: nombre,
+        contraseña: contraseña
+    };
+}
+
+function guardarUsuarioEnLocalStorage(usuario) {
+    const usuariosGuardados = JSON.parse(localStorage.getItem('usuarios')) || [];
+    usuariosGuardados.push(usuario);
+    localStorage.setItem('usuarios', JSON.stringify(usuariosGuardados));
+}
+
+// Iniciar sesión con usuarios creados
+const btnIngresar = document.getElementById("btn-ingresar");
+btnIngresar.addEventListener("click", () => {
+    const usuarioIngreso = document.getElementById("usuarioIngreso").value.trim();
+    const contraseñaIngreso = document.getElementById("contraseñaIngreso").value.trim();
+
+    if (validarCredenciales(usuarioIngreso, contraseñaIngreso)) {
+        // Aquí puedes redirigir o realizar otras acciones después de iniciar sesión correctamente
+        console.log("Inicio de sesión exitoso");
+    } else {
+        // Mostrar mensaje de error de inicio de sesión
+        console.log("Credenciales incorrectas");
     }
 });
+
+// Validar usuario y contraseña ingresadas
+const validarCredenciales = (usuario, contraseña) => {
+    const usuariosGuardados = JSON.parse(localStorage.getItem('usuarios')) || [];
+    const usuarioEncontrado = usuariosGuardados.find(usr => usr.nombre === usuario);
+
+    return usuarioEncontrado && usuarioEncontrado.contraseña === contraseña;
+};
