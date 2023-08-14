f519044fabf2f7291a2c76e287d07245cfcede84
agus <agu.rostagno@hotmail.com>
2023-08-14 11:36:59 -0300

    fullpage

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,17 @@
 const landing = new fullpage('#fullpage', {
-	
+
+    //opciones scroll
+    css3: true,
+	autoScrolling: true,
+	fitToSection: false,
+	fitToSectionDelay: 700,
+    scrollBar: false,
+    easing: 'easeInOutCubic',
+    easingcss3: 'ease-out',
+    loopBottom: true,
+
+    //opciones diseño
+    waterEffect: false,
+	waterEffectOptions: { animateContent: true, animateOnMouseMove: true},
+
 });
